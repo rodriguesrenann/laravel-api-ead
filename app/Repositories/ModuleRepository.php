4b1ab4657250e5a46273cc910c070c50773fd4fa
Renan Rodrigues <renanrodrigues_cb_@hotmail.com>
2022-03-01 22:11:54 -0300

    corrigindo tipagem

--- app/Repositories/ModuleRepository.php
+++ app/Repositories/ModuleRepository.php
@@ -3,18 +3,19 @@
 namespace App\Repositories;
 
 use App\Models\Module;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 class ModuleRepository
 {
-    private Model $model;
+    private Module $model;
 
     public function __construct(Module $model)
     {
         $this->model = $model;
     }
 
-    public function getModulesByCourseId($id)
+    public function getModulesByCourseId($id): Collection
     {
         return $this->model->where('course_id', $id)->get();
     }
